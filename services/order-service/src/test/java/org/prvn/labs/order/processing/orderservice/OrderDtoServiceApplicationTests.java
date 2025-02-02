5966a39b0e2ac1e5a965dc81f208bba76c71f1f3
Praveen Are <arewar.praveen@gmail.com>
2025-02-02 13:07:21 +0900

    added test, mail classes and pom for order service. closes #3

---
+++ services/order-service/src/test/java/org/prvn/labs/order/processing/orderservice/OrderDtoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prvn.labs.order.processing.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDtoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
